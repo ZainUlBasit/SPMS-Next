9b02181f4e33cc0b658997a455926a588b70b1a6
ZainUlBasit <zainulbasit486@gmail.com>
2024-04-17 17:08:57 +0500

    chnages

--- components/Navigations/CompanyNavs.jsx
+++ components/Navigations/CompanyNavs.jsx
@@ -8,7 +8,7 @@ import { TbFileReport } from "react-icons/tb";
 
 const CompanyNavs = () => {
   return (
-    <div className="flex justify-center items-center gap-x-3 flex-wrap">
+    <div className="flex justify-center items-center gap-x-3">
       <NavigationsBtn
         Title={"Create New"}
         Icon={BsBuildingAdd}
--- components/Navigations/ItemNavs.jsx
+++ components/Navigations/ItemNavs.jsx
@@ -8,7 +8,7 @@ import { TbFileReport, TbLayoutGridAdd } from "react-icons/tb";
 
 const ItemNavs = () => {
   return (
-    <div className="flex justify-center items-center gap-x-3 flex-wrap">
+    <div className="flex justify-center items-center gap-x-3">
       <NavigationsBtn
         Title={"Create New"}
         Icon={BsBuildingAdd}
